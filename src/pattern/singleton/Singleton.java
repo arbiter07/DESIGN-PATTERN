cf48ab8e1e826e405b4c06d40aebd741fc7b2781
arbiter <arbiter08@naver.com>
2025-03-14 15:12:47 +0900

    file location update

--- src/pattern/singleton/Singleton.java
+++ src/pattern/singleton/Singleton.java
@@ -1,4 +1,4 @@
-package pattern;
+package pattern.singleton;
 
 public class Singleton {
   private static volatile Singleton instance;
@@ -6,14 +6,14 @@ public class Singleton {
   private Singleton() {}
 
   public static Singleton getInstance() {
-      if (instance == null) { 
-          synchronized (Singleton.class) {
-              if (instance == null) {
-                  instance = new Singleton();
-              }
-          }
+    if (instance == null) { 
+      synchronized (Singleton.class) {
+        if (instance == null) {
+          instance = new Singleton();
+        }
       }
-      return instance;
+    }
+    return instance;
   }
   public static void main(String[] args) {
     Singleton instance1 = Singleton.getInstance();
